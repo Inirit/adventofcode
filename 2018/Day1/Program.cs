ca6b037730be960e2fe755a9097179e78fbaf980
Inirit <gbbrinzea@gmail.com>
2018-12-04 22:08:55 -0800

    Day 1, part 1 and part 2

--- 2018/Day1/Program.cs
+++ 2018/Day1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day1
 {
@@ -6,7 +9,54 @@ namespace Day1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var values = GetValues(@".\input\input.txt");
+			var aggregateValue = Part1(values);
+			var firstDuplicate = Part2(values);
+
+			Console.WriteLine($"Results: [Value Count: {values.Length}] [Aggregate Value: {aggregateValue}] [First Duplicate: {firstDuplicate}]");
+
+			Console.WriteLine("Press enter to continue...");
+
+			Console.ReadLine();
+		}
+
+		static int[] GetValues(string filePath)
+		{
+			var values = File.ReadAllLines(filePath)
+				.Select(line => int.Parse(line.Trim()))
+				.ToArray();
+
+			return values;
+		}
+
+		static int Part1(IEnumerable<int> values)
+		{
+			var result = values.Aggregate((x, y) => x + y);
+
+			return result;
+		}
+
+		static int Part2(int[] values)
+		{
+			var numbersEncountered = new HashSet<int>();
+			var duplicateFound = false;
+			var currentValue = 0;
+
+			for (var i = 0; i < values.Length && !duplicateFound; i++, i %= values.Length)
+			{
+				currentValue += values[i];
+
+				if (numbersEncountered.Contains(currentValue))
+				{
+					duplicateFound = true;
+				}
+				else
+				{
+					numbersEncountered.Add(currentValue);
+				}
+			}
+
+			return currentValue;
 		}
 	}
 }
